726a73c1a6be721f7c39130d15bc18dde9ffc0fc
wangguangwu <wangguangwu@pukanghealth.com>
2023-05-04 16:42:15 +0800

    init

---
+++ src/main/java/com/wangguangwu/keepknowing/KeepKnowingApplication.java
@@ -0,0 +1,13 @@
+package com.wangguangwu.keepknowing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepKnowingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepKnowingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wangguangwu/keepknowing/KeepKnowingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangguangwu.keepknowing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepKnowingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
